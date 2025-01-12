129d51d4f9b83afd780e2fb8104265dc0e2345d9
gursheyss <rkwarya@gmail.com>
2025-01-12 09:34:14 -0800

    fix: change to ts config and add superwall dep

---
+++ app.config.ts
@@ -0,0 +1,74 @@
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: "wisp-app",
+  slug: "wisp-app",
+  owner: "productstudioinc",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.productstudio.wisp",
+    usesAppleSignIn: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    package: "com.productstudio.wisp"
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      }
+    ],
+    "expo-apple-authentication",
+    "expo-localization",
+    [
+      "expo-build-properties",
+      {
+        "android": {
+          "minSdkVersion": 26
+        }
+      }
+    ],
+    "expo-font"
+  ],
+  experiments: {
+    typedRoutes: true
+  },
+  extra: {
+    router: {
+      origin: false
+    },
+    eas: {
+      projectId: "3d62779f-23ce-4e87-b81f-c2735e905cdb"
+    }
+  }
+};
+
+export default config;
