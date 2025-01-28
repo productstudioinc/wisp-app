f3379f1836762dd3220c0e9c4351286feef88334
Dillion Verma <hello@dillion.io>
2025-01-28 13:41:51 -0500

    feat: add onboarding screen back

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,7 @@
-import { cssInterop } from 'nativewind';
 import { MaterialIcons } from '@expo/vector-icons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useRouter } from 'expo-router';
+import { cssInterop } from 'nativewind';
 import React from 'react';
-import { Linking, Text, TouchableOpacity, View } from 'react-native';
+import { Linking, Text, View } from 'react-native';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Auth } from '~/components/Auth.native';
